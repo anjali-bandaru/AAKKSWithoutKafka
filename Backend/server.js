0b25430b8dcfc886d0a5a0c04e7f78aff2531b83
Anjali <anjali.abndaru@sjsu.edu>
2019-11-19 18:30:40 -0800

    Base version

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ var mongoose = require('mongoose');
 //var connStr = config.database_type + '://' + config.database_username + ':' + config.database_password + '@' + config.database_host + ':' + config.database_port + '/' + config.database_name;
 var connStr = config.connection_string;
 console.log(connStr);
-mongoose.connect(connStr, { useNewUrlParser: true, poolSize: 10, }, function(err) {
+mongoose.connect(connStr, { useNewUrlParser: true, poolSize: 0, }, function(err) {
   if (err) throw err;
   else {
       console.log('Successfully connected to MongoDB');
